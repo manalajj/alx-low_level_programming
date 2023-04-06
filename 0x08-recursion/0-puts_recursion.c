29730e064fa31cc365b1949f0f46c55503eea163
ajan manal <manal.ajan1706@gmail.com>
2023-04-05 18:34:56 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,12 @@
  *@s: input
  *Return: Always 0 (Success)
  */
-void _puts_recursion(char*s)
+void _puts_recursion(char *s)
 {
 if (*s)
 {
-_putchar (*s);
-_puts_recursion (s + 1);
+_putchar(*s);
+_puts_recursion(s + 1);
 }
 else
 _putchar('/n');
