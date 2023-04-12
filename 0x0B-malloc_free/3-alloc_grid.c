ae78d9e238069bbc6e4e41d555457d48500286aa
ajan manal <manal.ajan1706@gmail.com>
2023-04-12 10:49:44 -0700

    alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,10 +1,35 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
  */
-int _islower(int c)
+int **alloc_grid(int width, int height)
 {
-return (c >= 'a' && c <= 'z');
+int **mee;
+int x, y;
+if (width <= 0 || height <= 0)
+return (NULL);
+mee = malloc(sizeof(int *) * height);
+if (mee == NULL)
+return (NULL);
+for (x = 0; x < height; x++)
+{
+mee[x] = malloc(sizeof(int) * width);
+if (mee[x] == NULL)
+{
+for (; x >= 0; x--)
+free(mee[x]);	
+free(mee);
+return (NULL);
+}
+}
+for (x = 0; x < height; x++)
+{
+for (y = 0; y < width; y++)
+mee[x][y] = 0;
+}
+return (mee);
 }
